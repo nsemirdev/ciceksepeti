3fe5946c325fd1c877370336f01010d069df652f
nsemirdev <nsemirdev@gmail.com>
2023-01-21 02:50:52 +0300

    initial xcode project

---
+++ ciceksepeti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ciceksepeti
+//
+//  Created by Emir Alkal on 21.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
